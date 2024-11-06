31f73366794d3a875f052cd3ca412e4eb55070ce
danyadji <danyadji90@gmail.com>
2024-11-06 14:54:04 +0700

    first commit

---
+++ Web_Porto/assets/js/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'button': '#008c9e', // Replace with your hex code
+        },
+      },
+    },
+  };
+  
